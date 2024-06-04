362c925c66fc02456ca78652a242fb93e0ec82e4
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-06-04 23:02:16 +0300

    test: add dropdown demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import './App.css'
 
 import { Layer, Rect, Stage } from 'react-konva'
 
+import DropdownMenuDemo from './vendor/components/DropdownMenu/DropdownMenu'
+
 function App() {
   return (
     <>
-      <Stage width={window.innerWidth} height={window.innerHeight} className="stage">
-        <Layer>
-          <Rect draggable fill="red" width={100} height={100} />
-        </Layer>
-      </Stage>
+      <div>
+        <DropdownMenuDemo className="dropPosition" />
+        <Stage draggable width={window.innerWidth} height={window.innerHeight} className="stage">
+          <Layer>
+            <Rect draggable fill="red" width={100} height={100} />
+          </Layer>
+        </Stage>
+      </div>
     </>
   )
 }
