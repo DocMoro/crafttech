b6901b88412067eb2ab15be9a7e35fa7f29d85f0
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-06-04 12:02:16 +0300

    test: add stage and layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import './App.css'
 
-import { useState } from 'react'
+import { Layer, Rect, Stage } from 'react-konva'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Stage width={window.innerWidth} height={window.innerHeight} className="stage">
+        <Layer>
+          <Rect draggable fill="red" width={100} height={100} />
+        </Layer>
+      </Stage>
     </>
   )
 }
